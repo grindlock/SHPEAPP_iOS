3c89fa1eacceb099a2a6b92c572cd706952e4af2
grindlock <sergioenriqueperez@gmail.com>
2017-04-06 11:12:25 -0400

    email and password checks with an alert is done. Other cosmetic code added.

--- shpe_test/MainScreenVC.swift
+++ shpe_test/MainScreenVC.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class MainScreenVC: UIViewController {
+   
     @IBOutlet weak var email: UITextField!
     
     @IBOutlet weak var password: UITextField!
     
+    @IBOutlet weak var emailLbl: UILabel!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let label = UILabel(frame: CGRect(x:0,y:0, width: 120, height: 30))
+        label.font = email.font
+        label.text = "@knights.ucf.edu"
+        
         email.returnKeyType = .next
         email.delegate = self
         password.returnKeyType = .done
         password.delegate = self
+        
+        email.rightView = label
+        email.rightViewMode = .always
+        
+        
         
     }
     
@@ -26,13 +41,35 @@ class MainScreenVC: UIViewController {
 
     @IBAction func pressedLogin(_ sender: Any) {
         
+        if ValidationFunctions().emailIsEmpty(email: email.text!, controller: self){
+            email.borderWidth = 2
+            email.cornerRadius = 5
+            email.borderColor = UIColor.red
+            
+            
+        }
+        if !ValidationFunctions().passwordIsValid(textField: password.text!, controller: self){
+            password.borderWidth = 2
+            password.cornerRadius = 5
+            password.borderColor = UIColor.red
+
+        }
+            
+        else{
+
+            performSegue(withIdentifier: "login", sender: "")
+        }
+        
+        
         //func call to check email validity
         //func call to check password meets the required meet
         
         //login to the server
         
         //if state
-        performSegue(withIdentifier: "login", sender: "")
+      
+            
+            //performSegue(withIdentifier: "login", sender: "")
         
     }
     
@@ -47,13 +84,6 @@ class MainScreenVC: UIViewController {
         
         return true
     }
-    
-    
-
-    
-    
-    
-
 
 }
 
---
+++ shpe_test/ValidationFunctions.swift
@@ -0,0 +1,80 @@
+//
+//  ValidationFunctions.swift
+//  shpe_test
+//
+//  Created by Sergio Perez-Aponte on 3/24/17.
+//  Copyright © 2017 Sergio Perez-Aponte. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ValidationFunctions{
+    
+    //var message:String = "", title:String = ""
+    
+    
+    func emailIsEmpty(email:String, controller:UIViewController)->Bool{
+    
+    if email.isEmpty == true{
+        errorsMessages(error: 1, controller: controller)
+        return true
+    }
+        return false
+}
+
+func passwordIsValid(textField:String, controller:UIViewController)->Bool{
+// ((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$&]).{8,})  use this one for password. It checks for the char listed and length between 8 and 16
+// ^[0-9a-zA-Z\.\_]{2,}$  thi one is good for email
+    let length: Int = textField.characters.count
+    
+    let RegEx = "((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$&]).{8,})"
+    let Test = NSPredicate(format: "SELF MATCHES %@", RegEx)
+    //let flag = Test.evaluate(with: textField)
+   
+    if length < 7 || length > 17 || textField.isEmpty {
+        errorsMessages(error: 2, controller: controller)
+        return false
+    }
+    else if (!Test.evaluate(with: textField)){ //flag == false) {
+        errorsMessages(error: 3, controller: controller)
+        return false
+    }
+    
+    return true
+}
+
+/*func passwordSpecials(textField:String)->Bool{
+
+    if textField.contains("!#$%^&*"){
+        return true
+    }
+    
+    return false
+
+}*/
+
+func errorsMessages(error:Int, controller:UIViewController){
+        var message:String = ""
+        var title:String!
+    switch(error){
+    case 1: title="Email"
+        message += "Please enter an UCF email"
+    case 2: title = "Password"
+    message = "Password is empty or to short. The length needs to be between 8 and 16 characters."
+    case 3: title = "Password"
+        message = "Password needs at least 1 Uppercase letter, at least 1 number and at least 1 special character (!@#$&)."
+    default:message = ""
+    
+    }
+        showAlert(title: title ,msg: message , controller: controller)
+   
+}
+    
+    func showAlert(title: String, msg: String, controller: UIViewController) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        controller.present(alert, animated: true, completion: nil)
+    }
+    
+}
